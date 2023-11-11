9ae26e829936df950020a2c0393b38efa298564c
Antas Sachan <antassachan123@gmail.com>
2023-11-11 10:24:26 +0530

    s1

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,20 +1,14 @@
-// Home.js
 import React from 'react';
-import p1 from "../images/pic.jpg";
-import p2 from "../images/pic2.jpeg";
-import p3 from "../images/pic3.jpg";
-import styles from './Login.module.css'; // Import the CSS module
+import styles from './Login.module.css'; 
 import bgvd from "../videos/vdo.mp4"
 
 const Home = () => {
   return (
-    <div className={styles.container}>
-    <video autoPlay loop muted style={{height:"100vh", width:"100%"}} >
-      <source src={bgvd} type='video/mp4'/>
-    </video>
-      {/* <img src={p1} alt="" className={styles.image} />
-      <img src={p2} alt="" className={styles.image} />
-      <img src={p3} alt="" className={styles.image} /> */}
+    <div >
+    <video src={bgvd} autoPlay loop muted style={{height:"100vh", width:"100%", objectFit:"cover"}}/>
+      <div className="text" style={{position:"absolute", color:"white", top:"250px",right:"350px", textAlign:"center" }} >
+        <h1>WELCOME TO THE DASHBOARD OF <br></br>  <span style={{color:"lightskyblue", fontSize:"100px" }} >SIEMENS</span> </h1>
+      </div>
     </div>
   );
 };
